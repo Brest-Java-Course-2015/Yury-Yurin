41e0c90388e585d4c4362a461246307c6110fdc5
Yury-Yurin <yhra@yandex.ru>
2015-10-02 19:50:28 +0300

    first programm

---
+++ src/com/brest/epam/courses/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by blondeks on 2.10.15.
+ */
+package com.brest.epam.courses;
+public class Main {
+    public static void main(String[] args) {
+        int i=0;
+        System.out.println(" test ");
+        i++;
+        System.out.println(" test " + i);
+    }
+}
