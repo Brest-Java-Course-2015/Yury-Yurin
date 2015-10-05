efbb6a5bcb7789b9e4e59557730d5b0c5213a2ce
Yury-Yurin <yhra@yandex.ru>
2015-10-05 20:45:13 +0300

    change pom files

---
+++ app-core/test/java/com/epam/brest/course2015/domain/UserTest.java
@@ -0,0 +1,10 @@
+package com.epam.brest.course2015.domain;
+
+import static org.junit.Assert.*;
+public class UserTest {
+
+    @org.junit.Test
+    public void testGetLogin() throws Exception {
+
+    }
+}
