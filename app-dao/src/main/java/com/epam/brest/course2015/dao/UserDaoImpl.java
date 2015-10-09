df9041efc0e47ab2b3cd864b90828996f79b71b6
Yury-Yurin <yhra@yandex.ru>
2015-10-09 20:43:56 +0300

    Add property file for sql, add logs

--- app-dao/src/main/java/com/epam/brest/course2015/dao/UserDaoImpl.java
+++ app-dao/src/main/java/com/epam/brest/course2015/dao/UserDaoImpl.java
@@ -2,20 +2,35 @@ package com.epam.brest.course2015.dao;
 
 import com.epam.brest.course2015.dao.rowmappers.UserMapper;
 import com.epam.brest.course2015.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class UserDaoImpl implements UserDao {
 
     private UserMapper userMapper = new UserMapper();
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Value("${user.select}")
+    private String userSelect;
+
+    @Value("${user.selectById}")
+    private String userSelectById;
+
+    @Value("${user.addUser}")
+    private String addUser;
+
+    @Value("${user.deleteUser}")
+    private String deleteUser;
 
     public UserDaoImpl(DataSource dataSource) {
         jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
@@ -23,21 +38,22 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        return jdbcTemplate.query("select * from user", userMapper);
+        return jdbcTemplate.query(userSelect, userMapper);
     }
 
     @Override
     public User getUserById(Integer id) {
+        LOGGER.info("id: {}", id);
         Map<String, Object> hashMap = new HashMap<String, Object>();
         hashMap.put("id", id);
-        return jdbcTemplate.queryForObject("select * from user where userid=:id", hashMap, userMapper);
+        return jdbcTemplate.queryForObject(userSelectById, hashMap, userMapper);
     }
 
     @Override
     public void deleteUser(String login) {
         Map<String, String> hashMap = new HashMap<String, String>();
         hashMap.put("login", login);
-        jdbcTemplate.update("delete from user where login=:login", hashMap);
+        jdbcTemplate.update(deleteUser, hashMap);
     }
 
     @Override
@@ -46,7 +62,6 @@ public class UserDaoImpl implements UserDao {
         hashMap.put("id",id);
         hashMap.put("login",login);
         hashMap.put("password",password);
-        jdbcTemplate.update("insert into user(userId,login,password) values(:id,:login,:password)",hashMap);
-
+        jdbcTemplate.update(addUser,hashMap);
     }
 }
