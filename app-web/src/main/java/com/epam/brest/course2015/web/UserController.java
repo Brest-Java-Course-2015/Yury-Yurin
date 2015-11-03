3bcde55479ddaaf275e69fcc8cb7abc552d57e85
blondeks <yhra@yandex.ru>
2015-11-04 01:03:18 +0300

    clean workspace for new project

--- app-web/src/main/java/com/epam/brest/course2015/web/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.epam.brest.course2015.web;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class UserController {
-    private static final Logger LOGGER = LogManager.getLogger();
-
-}
